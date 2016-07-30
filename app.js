025103f7b1da28ade3aba0009b4445ec00a4953f
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2016-07-30 14:37:02 +1000

    First attempt querying results

--- app.js
+++ app.js
@@ -11,34 +11,36 @@
 'use strict';
 
 const
-  bodyParser = require('body-parser'),
-  express = require('express'),
-  https = require('https'),
-  envVars = require('./lib/envVars.js'),
-  messengerGeneric = require('./lib/messengerGeneric.js'),
-  botLoop = require('./lib/botLoop.js');
+    bodyParser = require( 'body-parser' ),
+    express = require( 'express' ),
+    https = require( 'https' ),
+    envVars = require( './lib/envVars.js' ),
+    messengerGeneric = require( './lib/messengerGeneric.js' ),
+    botLoop = require( './lib/botLoop.js' );
 
 var app = express();
-app.set('port', process.env.PORT || 3000);
-app.set('view engine', 'ejs');
-app.use(bodyParser.json({ verify: messengerGeneric.verifyRequestSignature }));
-app.use(express.static('public'));
+app.set( 'port', process.env.PORT || 3000 );
+app.set( 'view engine', 'ejs' );
+app.use( bodyParser.json( {
+    verify: messengerGeneric.verifyRequestSignature
+} ) );
+app.use( express.static( 'public' ) );
 
 /*
  * Use your own validation token. Check that the token used in the Webhook
  * setup is the same token used here.
  *
  */
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === envVars.VALIDATION_TOKEN) {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);
-  }
-});
+app.get( '/webhook', function ( req, res ) {
+    if ( req.query[ 'hub.mode' ] === 'subscribe' &&
+        req.query[ 'hub.verify_token' ] === envVars.VALIDATION_TOKEN ) {
+        console.log( "Validating webhook" );
+        res.status( 200 ).send( req.query[ 'hub.challenge' ] );
+    } else {
+        console.error( "Failed validation. Make sure the validation tokens match." );
+        res.sendStatus( 403 );
+    }
+} );
 
 
 /*
@@ -48,55 +50,55 @@ app.get('/webhook', function(req, res) {
  * https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
  *
  */
-app.post('/webhook', function (req, res) {
-  var data = req.body;
+app.post( '/webhook', function ( req, res ) {
+    var data = req.body;
 
-  // Make sure this is a page subscription
-  if (data.object == 'page') {
-    // Iterate over each entry
-    // There may be multiple if batched
-    data.entry.forEach(function(pageEntry) {
+    // Make sure this is a page subscription
+    if ( data.object == 'page' ) {
+        // Iterate over each entry
+        // There may be multiple if batched
+        data.entry.forEach( function ( pageEntry ) {
 
-      //process the event
-      botLoop.processEvent(pageEntry);
-    });
+            //process the event
+            botLoop.processEvent( pageEntry );
+        } );
 
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know you've
-    // successfully received the callback. Otherwise, the request will time out.
-    res.sendStatus(200);
-  }
-});
+        // Assume all went well.
+        //
+        // You must send back a 200, within 20 seconds, to let us know you've
+        // successfully received the callback. Otherwise, the request will time out.
+        res.sendStatus( 200 );
+    }
+} );
 
 /*
  * This path is used for account linking. The account linking call-to-action
  * (sendAccountLinking) is pointed to this URL.
  *
  */
-app.get('/authorize', function(req, res) {
-  var accountLinkingToken = req.query['account_linking_token'];
-  var redirectURI = req.query['redirect_uri'];
+app.get( '/authorize', function ( req, res ) {
+    var accountLinkingToken = req.query[ 'account_linking_token' ];
+    var redirectURI = req.query[ 'redirect_uri' ];
 
-  // Authorization Code should be generated per user by the developer. This will
-  // be passed to the Account Linking callback.
-  var authCode = "1234567890";
+    // Authorization Code should be generated per user by the developer. This will
+    // be passed to the Account Linking callback.
+    var authCode = "1234567890";
 
-  // Redirect users to this URI on successful login
-  var redirectURISuccess = redirectURI + "&authorization_code=" + authCode;
+    // Redirect users to this URI on successful login
+    var redirectURISuccess = redirectURI + "&authorization_code=" + authCode;
 
-  res.render('authorize', {
-    accountLinkingToken: accountLinkingToken,
-    redirectURI: redirectURI,
-    redirectURISuccess: redirectURISuccess
-  });
-});
+    res.render( 'authorize', {
+        accountLinkingToken: accountLinkingToken,
+        redirectURI: redirectURI,
+        redirectURISuccess: redirectURISuccess
+    } );
+} );
 
 // Start server
 // Webhooks must be available via SSL with a certificate signed by a valid
 // certificate authority.
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.listen( app.get( 'port' ), function () {
+    console.log( 'Node app is running on port', app.get( 'port' ) );
+} );
 
 module.exports = app;
---
+++ test-scraper.js
@@ -0,0 +1,50 @@
+const request = require( 'request' ).defaults( {
+    json: true,
+} );
+
+const COOORD_WIDTH = 10;
+var coords = {
+    // lat: -27.468217,
+    // lng: 153.0590176,
+
+    // nsw
+    // lat: -29.113775,
+    // lng: 153.325195,
+
+    // bne
+    lat: -27.468217,
+    lng: 153.0590176,
+};
+
+var b = {
+    "mid": "mid.1469851462949:7d941e2bfb608a9989",
+    "seq": 57,
+    "attachments": [ {
+        "title": "Pablo's Location",
+        "url": "https://www.facebook.com/l.php?u=https%3A%2F%2Fwww.bing.com%2Fmaps%2Fdefault.aspx%3Fv%3D2%26pc%3DFACEBK%26mid%3D8100%26where1%3D-27.497312%252C%2B153.015008%26FORM%3DFBKPL1%26mkt%3Den-US&h=OAQHHo_hN&s=1&enc=AZN6jVdV0FmOtl02IrhuWBoZ2myVGQy37ROzhWmrbamjFhoch9GyyVIw_vp8FTeKoaxYtC7Aqx5mKVcCGCSzR1Rphtpk7_dxyK-OWXOvINRctg",
+        "type": "location",
+        "payload": {
+            "coordinates": {
+                "lat": -27.497312,
+                "long": 153.015008
+            }
+        }
+    } ]
+};
+
+var qs = {
+    key: 'WMGj4AkJOg+iLtT8qhha',
+    // address: '24 Bruce Road Glenbrook NSW 2773',
+    // radius: 400,
+    top_right_lat: coords.lat + COOORD_WIDTH / 2,
+    top_right_lng: coords.lng + COOORD_WIDTH / 2,
+    bottom_left_lat: coords.lat - COOORD_WIDTH / 2,
+    bottom_left_lng: coords.lng - COOORD_WIDTH / 2,
+};
+console.log( qs );
+request.get( 'https://api.planningalerts.org.au/applications.js', {
+    qs: qs
+}, ( error, response, body ) => {
+    if ( error ) throw error;
+    console.log( JSON.stringify( body, null, '    ' ) );
+} );
